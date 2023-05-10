62be10c1b9672b5fa5da9bbfbb0f7810467d2f18
emPeeGee <eempeegee@gmail.com>
2023-05-10 18:47:28 +0300

    feat:  🍊  Allow any origin cors

--- internal/cors/middleware.go
+++ internal/cors/middleware.go
@@ -9,7 +9,7 @@ import (
 
 func Handler() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
